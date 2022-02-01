ae8b755a8461870c6565e01fcd7a5c276011c8ab
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-31 20:37:26 -0600

    funcionalidad de precio con descuento

--- descuentos.js
+++ descuentos.js
@@ -10,8 +10,19 @@ function calcularPrecioConDescuento(precio, descuento){
     return preciosConDescuento;
 }
 
+onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDescount.value;
+
+    const precioConDescuento = calculaPrecioConDescuento(priceValue, descountValue);
+   const resultP = document.getElementById("ResultP");
+    resultP.innertest = `El precio con descuento son $ ${precioConDescuento}`
+}
 
 
-console.log(`el precio con descuento es ${calcularPrecioConDescuento(120, 15)}`)
+
+console.log(`el precio con descuento es ${calcularPrecioConDescuento(120, 15)}`);
 
 

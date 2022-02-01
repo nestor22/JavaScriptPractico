deb970c55b2399e2e6618238b54042eba4d7a657
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-31 20:42:21 -0600

    correccion de error en descuentos

--- descuentos.js
+++ descuentos.js
@@ -8,21 +8,17 @@ function calcularPrecioConDescuento(precio, descuento){
     
     const preciosConDescuento = (precio * porcentajePresioConDescuento)/100;
     return preciosConDescuento;
-}
+};
 
-onClickButtonPriceDiscount(){
+function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
     const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDescount.value;
+    const discountValue = inputDiscount.value;
 
-    const precioConDescuento = calculaPrecioConDescuento(priceValue, descountValue);
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
    const resultP = document.getElementById("ResultP");
-    resultP.innertest = `El precio con descuento son $ ${precioConDescuento}`
+    resultP.innerText = `El precio con descuento son $ ${precioConDescuento}`
 }
-
-
-
-console.log(`el precio con descuento es ${calcularPrecioConDescuento(120, 15)}`);
 
 

0d4167738f9f5589a9730199cc5bd9a92a4b0b6f
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-02-01 22:22:56 -0600

    calculos de top salarios medias

--- analisis.js
+++ analisis.js
@@ -1,34 +1,46 @@
-const salarioscol = colombia.map((per) => {
-  return per.salarioscol;
-});
-
-salarioscolSorted = salarioscol.sort((salaryA, salaryB) => {
-  return salaryA - salaryB;
-});
-
+//Helpers o Utils
 function esPar(numerito) {
   return numerito % 2 === 0;
 }
 
-function calcularMediaAritmetica (lista){
-    
-     const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento = lista.length) {
-        return valorAcumulado + nuevoElemento;
+function calcularMediaAritmetica(lista) {
+  const sumaLista = lista.reduce(function (
+    valorAcumulado = 0,
+    nuevoElemento = lista.length
+  ) {
+    return valorAcumulado + nuevoElemento;
+  });
+  const promedioLista = sumaLista / lista.length;
 
-    })
-    const promedioLista = sumaLista / lista.length;
-
-    return promedioLista;
+  return promedioLista;
 }
 
+//salarios
+const salarioscol = colombia.map((per) => {
+  return per.salarioscol;
+});
+salarioscolSorted = salarioscol.sort((salaryA, salaryB) => {
+  return salaryA - salaryB;
+});
 
+//mediana General 
 function medianaSalariosCol(lista) {
-    const mitad = parseInt(lista.length / 2);
-    if(esPar(lista.length)){
-        let val1 = lista[mitad-1];
-        let val2 = lista[mitad];
-        return (val1 + val2)/2;
-    }else{
-        return lista[mitad]
-    }
+  const mitad = parseInt(lista.length / 2);
+  if (esPar(lista.length)) {
+    let val1 = lista[mitad - 1];
+    let val2 = lista[mitad];
+    return (val1 + val2) / 2;
+  } else {
+    return lista[mitad];
+  }
 }
+
+const spliceStart = (salarioscolSorted.length * 90)/100;
+const spliceCount = salarioscolSorted.length - spliceStart;
+const salariosTop10 = salarioscolSorted.splice(spliceStart, spliceCount);
+
+const medianaGeneralCol = medianaSalariosCol(salarioscol);
+const medianaTop10Col = medianaSalariosCol(salariosTop10);
+
+console.log(`Salarios generales de colombia ${medianaGeneralCol}`);
+console.log(`Salarios top 10 de colombia ${medianaGeneralCol}`);
